aebcc5c47774dfb01ea04650e444b3246129a543
sebaUC <sebastian.derpsch@uc.cl>
2023-09-07 21:15:50 -0300

    ahora siuu

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'main_view/index'
+
+  root 'main_view#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
