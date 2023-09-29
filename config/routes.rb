1d2b01707ad8cabbf32290859c05d871a6883d65
mailatorree <maite.latorre@uc.cl>
2023-09-29 02:26:16 -0300

    conexion con otros usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :likes, only: [:index] # Define la ruta para la acción 'index' de Likes.
   resources :calcetines, only: [:index]
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
 
   post '/likes', to: 'likes#index'
   post '/calcetines', to: 'calcetines#index'
@@ -14,8 +14,9 @@ Rails.application.routes.draw do
   # config/routes.rb
   get 'users/profile', to: 'users#profile', as: 'user_profile'
 
-
-
+  # Ruta para ver el perfil de un usuario por su ID
+  get 'users/:id', to: 'users#show', as: 'view_user_profile'
+  get 'search', to: 'users#search', as: 'user_search'
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
