309fb36089b2a8bfbe7018d95dddde941e9119fa
Isidora <ichavezc@uc.cl>
2023-10-30 20:38:29 -0300

    26% test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,12 +1,13 @@
 require 'test_helper'
 
-class UserTest < ActiveSupport::TestCase
+class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = User.new(
       email: "test@example.com",
       password: "password123",
       password_confirmation: "password123"
     )
+
   end
 
   test "email should be present" do
